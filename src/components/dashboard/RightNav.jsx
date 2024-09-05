58b724c52fac623619459ab849bfa7874ae06d35
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-05 22:44:58 +0500

    add logout functionality to dashboard

--- src/components/dashboard/RightNav.jsx
+++ src/components/dashboard/RightNav.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaCartPlus } from "react-icons/fa";
+import { useLogoutMutation } from "../../app/api/userSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const RightNav = () => {
+  const [cookie, setCookie] = useState();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    setCookie(token);
+  }, []);
+
+  const Navigate = useNavigate();
+  const [logout] = useLogoutMutation();
+
+  const handleLogOut = async () => {
+    try {
+      await logout({ credential: "include" }).unwrap();
+      toast.success("logout successfully");
+      Navigate("/");
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
   return (
     <div className="fixed top-0 w-full bg-[#0C1844] h-14 ">
       <div className=" h-full flex justify-evenly items-center">
@@ -19,7 +43,14 @@ const RightNav = () => {
           <button>
             <FaCartPlus size={20} />
           </button>
-          <button>logout</button>
+          {cookie && (
+            <button
+              onClick={handleLogOut}
+              className="border rounded-lg px-1 hover:bg-[#F9DBBA] hover:text-gray-700"
+            >
+              logout
+            </button>
+          )}
           <img
             className="w-8 rounded-full"
             src="https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png"
