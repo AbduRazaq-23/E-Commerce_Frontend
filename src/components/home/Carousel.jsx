11627c570cc9d11187daaae9f1e8630c41fa3ff3
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-08 21:01:39 +0500

    carousel design update

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -4,11 +4,11 @@ const Carousel = () => {
   return (
     <div className="w-full h-96 mt-16 bg-[#1A4870] text-gray-200">
       <div className="md:flex items-center h-full w-full gap-8">
-        <div className="flex-1 ">
-          <h1 className="text-3xl font-bold ml-12">
+        <div className="flex-1 p-3 md:p-10 ">
+          <h1 className="text-3xl font-bold  text-center   ">
             Wel<span className="text-[#F9DBBA]">Come</span>{" "}
           </h1>
-          <p className="ml-12">
+          <p className=" text-center">
             Find out what ecommerce is, along with the advantages and
             disadvantages for different ways of conducting business online.
           </p>
