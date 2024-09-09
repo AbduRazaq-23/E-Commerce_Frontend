4846d613c21a24b724ae430187e796d933369537
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-09 21:04:00 +0500

    color change

--- src/components/dashboard/GetProducts.jsx
+++ src/components/dashboard/GetProducts.jsx
@@ -34,7 +34,7 @@ const GetProducts = () => {
   };
 
   return (
-    <div className="w-full m-2 p-2 bg-[#F9DBBA] mx-auto rounded-lg text-gray-700 text-sm">
+    <div className="w-full m-2 p-2 bg-[#0C1844] mx-auto rounded-lg text-gray-200 text-sm">
       <h1 className="text-2xl font-bold text-center mb-5">products</h1>
       {isLoading && <h1>Loading...</h1>}
       {isError && <p>{error}</p>}
@@ -43,47 +43,51 @@ const GetProducts = () => {
           {products.map((product) => (
             <div
               key={product._id}
-              className="flex justify-between w-full bg-gray-200 my-1 p-2 rounded-lg"
+              className="flex justify-between w-full bg-[#1A4870] my-1 p-2 rounded-lg"
             >
               <div className="flex gap-4 items-center">
-                <img className="w-20 h-20" src={product.image} alt="image" />
+                <img
+                  className="w-32 h-32 rounded-md"
+                  src={product.image}
+                  alt="image"
+                />
                 <div className="flex flex-col gap-2">
                   <ul className="flex gap-2">
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       name: {product.name}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       dec: {product.description}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       brand: {product.brand}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       category: {product.category.name}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       quantity: {product.quantity}
                     </li>
                   </ul>
                   <ul className="flex gap-2">
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       $: {product.price}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       numRating: {product.numRating}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">
+                    <li className="bg-[#0C1844] p-1 rounded-lg">
                       numReview: {product.numReview}
                     </li>
-                    <li className="bg-gray-300 p-1 rounded-lg">reviews</li>
-                    <li className="bg-gray-300 p-1 rounded-lg">comment</li>
+                    <li className="bg-[#0C1844] p-1 rounded-lg">reviews</li>
+                    <li className="bg-[#0C1844] p-1 rounded-lg">comment</li>
                   </ul>
                 </div>{" "}
               </div>
 
               <div className="flex items-center gap-3 text-gray-200">
                 <Link to={`/dashboard/updateProducts/${product._id}`}>
-                  <button className="bg-blue-500 p-1 rounded-lg hover:bg-blue-600">
+                  <button className="bg-[#12215f] p-1 rounded-lg hover:bg-[#0C1844]">
                     update
                   </button>
                 </Link>
