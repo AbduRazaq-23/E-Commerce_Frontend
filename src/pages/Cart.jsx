b8d4ef595fa1c9bb14a617709e622f454c0a7700
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-16 20:42:41 +0500

    add to cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,24 +9,49 @@ const Cart = () => {
 
   const remove = (id) => dispatch(removeFromCart(id));
 
-  console.log(cartItem);
+  const totalPrice = cartItem.reduce((acc, cItem) => {
+    return acc + cItem.price;
+  }, 0);
+
   return (
     <div>
       <Navbar />
-      <div className="w-full min-h-screen bg-[#0C1844] p-10">
-        <div className="bg-[#3b446b] w-full md:w-[60%]  mt-16 p-6 text-gray-200 text-sm">
+      <div className="w-full min-h-screen bg-[#0C1844] p-10 md:flex">
+        <div className="bg-[#3b446b] w-full md:w-[80%] rounded-md   mt-16 p-6 text-gray-200 text-sm space-y-4">
           {cartItem?.map((product) => (
-            <div className="md:flex" key={product._id}>
-              <img className=" sm:w-30 sm:h-30" src={product.image} alt="img" />
-              <div>
-                <h1>{product.name}</h1>
-                <h1>name</h1>
-                <h1>name</h1>
-                <h1>name</h1>
+            <div
+              className="md:flex  p-2 justify-between rounded-md overflow-hidden border"
+              key={product._id}
+            >
+              <div className="md:flex  items-center ">
+                <img
+                  className="w-full md:w-44 h-44 rounded-md"
+                  src={product.image}
+                  alt="img"
+                />
+                <ul className="space-y-3 ml-5">
+                  <li className="font-bold text-2xl">{product.name}</li>
+                  <li>{product.rating.toFixed(1)}</li>
+                  <li>{product.brand}</li>
+                  <li>qty:{product.quantity}</li>
+                  <button
+                    className="border border-gray-400 rounded-md px-1 hover:scale-105"
+                    onClick={() => remove(product._id)}
+                  >
+                    delete from cart
+                  </button>
+                </ul>
               </div>
-              <button onClick={() => remove(product._id)}>delete</button>
+              <p>${product.price}</p>
             </div>
           ))}
+        </div>
+        {/* checkout  */}
+        <div className="bg-[#3b446b] mt-16  m-auto rounded-md p-6 text-gray-200 space-y-6">
+          <p>subTotal: ${totalPrice}</p>
+          <button className="w-full bg-[#0C1844] hover:scale-105 rounded-md px-2">
+            checkout
+          </button>
         </div>
       </div>
     </div>
